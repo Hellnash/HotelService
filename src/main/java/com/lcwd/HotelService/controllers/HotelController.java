e1689aafcb5b351e5dc731bed3873f054cbea1c3
Hellnash <shubhamraj3417@gmail.com>
2023-10-19 16:01:55 +0530

    Securing with Oauth2 authentication and setting up authorization standards using OKTA

--- src/main/java/com/lcwd/HotelService/controllers/HotelController.java
+++ src/main/java/com/lcwd/HotelService/controllers/HotelController.java
@@ -5,6 +5,7 @@ import com.lcwd.HotelService.services.HotelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,18 +18,21 @@ public class HotelController {
     @Autowired
     HotelService hotelService;
 
+    @PreAuthorize("hasAuthority('Admin')") //only admins can create the hotels
     @PostMapping
     public ResponseEntity<Hotel> createHotel(@RequestBody Hotel hotel){
         Hotel savedHotel = hotelService.createHotel(hotel);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedHotel);
     }
 
+    @PreAuthorize("hasAuthority('Admin') || hasAuthority('SCOPE_internal')") //admins and other microservice can get a hotel
     @GetMapping
     public ResponseEntity<List<Hotel>> findAllHotel(@RequestBody Hotel hotel){
         List<Hotel> allHotel = hotelService.getAllHotels();
         return ResponseEntity.status(HttpStatus.OK).body(allHotel);
     }
 
+    @PreAuthorize("hasAuthority('Admin') hasAuthority('SCOPE_internal') || hasAuthority('Users')") //anyone can get a hotel
     @GetMapping("/{hotelId}")
     public ResponseEntity<Hotel> getAHotel(@PathVariable String hotelId){
         Hotel foundHotel = hotelService.getHotel(hotelId);
